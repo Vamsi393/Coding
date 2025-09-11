2a46c1f5bc9ce2d234b123b701973492951f7ff4
Vamsi393 <vamsirajanarvk@gmail.com>
2025-09-11 16:06:25 +0530

    Update functions.c

--- functions.c
+++ functions.c
@@ -23,3 +23,26 @@ OUTPUT:enter the string:vamsikrishna
 length of the string is 12
   enter the string:vamsi krishna
 length of the string is 5```
+
+Q)IMPLEMENT A TO YOUR OWN STRING LENGTH MANIPULATION FUNCTION?
+A)#include<stdio.h>
+char astrcpy(char *destination,char *source);
+int main()
+{
+    char str1[10]="vamsi";
+    char str2[10];
+    astrcpy(str2,str1);
+    printf("%s",str2);
+}
+char astrcpy(char *destination,char *source)
+{
+    char *p=destination;
+    while(*destination=*source)
+    {
+        destination++;
+        source++;
+    }
+    return *p;
+}
+OUTPUT:vamsi
+    
