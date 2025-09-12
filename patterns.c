b522b4655ac49f0b8643ba55b2ee4fdd2b1b027f
Vamsi393 <vamsirajanarvk@gmail.com>
2025-09-12 22:47:29 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -186,6 +186,27 @@ int main()
     }
 }
 
+Q)
+A
+AB
+ABC
+ABCD
+A)#include<stdio.h>
+int main()
+{
+    int rows,cols,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(cols=1;cols<=rows;cols++)
+        {
+            printf("%c",cols+64);
+        }
+        printf("\n");
+    }
+}
+
 
 
 
