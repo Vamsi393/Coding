fd2a870d2830f3180cf5c38d3f3e77e2ff4a744a
Vamsi393 <vamsirajanarvk@gmail.com>
2025-09-12 22:30:32 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -140,6 +140,30 @@ int main()
     }
 }
 
+Q)
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+16 17 18 19 20 21 
+A)
+#include<stdio.h>
+int main()
+{
+    int rows,cols,n,count=1;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(cols=1;cols<=rows;cols++)
+        {
+            printf("%d ",count++);
+        }
+        printf("\n");
+    }
+}
+
 
 
 
