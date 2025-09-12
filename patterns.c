adf6beb395899714a498535773ade08753dc09c4
Vamsi393 <vamsirajanarvk@gmail.com>
2025-09-12 22:07:29 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -74,5 +74,28 @@ int main()
     }
 }
 
+Q)123456
+123456
+123456
+123456
+123456
+123456
+A)#include<stdio.h>
+int main()
+{
+    int rows,cols,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(cols=1;cols<=n;cols++)
+        {
+            printf("%d",cols);
+        }
+        printf("\n");
+    }
+}
+
+
 
 
