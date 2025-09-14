7673e5fd54361c7a3868209ffffb07b940bd8a25
Vamsi393 <vamsirajanarvk@gmail.com>
2025-09-14 23:27:49 +0530

    Update patterns.c

--- patterns.c
+++ patterns.c
@@ -1218,6 +1218,49 @@ int main()
     }
 }
 
+Q)
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+A)#include<stdio.h>
+int main()
+{
+    int rows,cols,spaces,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(rows=1;rows<=n;rows++)
+    {
+        for(spaces=1;spaces<=n-rows;spaces++)
+        {
+            printf(" ");
+        }
+        for(cols=1;cols<=2*rows-1;cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for(rows=1;rows<n;rows++)
+    {
+        for(spaces=1;spaces<=rows;spaces++)
+        {
+            printf(" ");
+        }
+        for(cols=1;cols<=2*(n-rows)-1;cols++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+
 
 
  
